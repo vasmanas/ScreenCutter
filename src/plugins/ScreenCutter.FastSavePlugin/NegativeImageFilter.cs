47ced586e5aeb371ba5d1565da98a9ea459e4aeb
vasmanas <andrius.vasmanas@gmail.com>
2017-03-03 09:19:26 +0200

    Added negative image filter.

---
+++ src/plugins/ScreenCutter.FastSavePlugin/NegativeImageFilter.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using ScreenCutter.PluginContract;
+
+namespace ScreenCutter.FastSavePlugin
+{
+    public class NegativeImageFilter : IImageFilter
+    {
+        public Bitmap Filter(Bitmap img)
+        {
+            var w = img.Width;
+            var h = img.Height;
+            var clone = new Bitmap(w, h, img.PixelFormat);
+
+            for (var i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    var colorToConvert = img.GetPixel(i, j);
+
+                    var invertedColor = Color.FromArgb((byte)~colorToConvert.R, (byte)~colorToConvert.G, (byte)~colorToConvert.B);
+
+                    clone.SetPixel(i, j, invertedColor);
+                }
+            }
+
+            return clone;
+        }
+    }
+}
